17a73e8eea19b124ea24133a7c36087698edd498
LittiSecond <breeze2882@yandex.ru>
2021-02-11 12:01:12 +0300

    Scene was changed, primitive character was addwd

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/CharacterOfPlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    [RequireComponent(typeof(UnitMotor))]
+    public sealed class CharacterOfPlayerController : MonoBehaviour
+    {
+        #region Fields
+
+        private Camera _camera;
+        private UnitMotor _motor;
+        private LayerMask _movementMask = 0x0200;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            _motor = GetComponent<UnitMotor>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f, _movementMask))
+                {
+                    _motor.MoveToPoint(hit.point);
+                }
+
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f, _movementMask))
+                {
+                    Debug.Log("CharacterOfPlayerController::Update: left mouse botton has been clicked.");
+                }
+
+            }
+
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+
+
+    }
+}
