826b8099a51f31c51ccfc8efcdb2afaeb69c916c
LittiSecond <breeze2882@yandex.ru>
2021-02-15 10:09:45 +0300

    PlayerStats was added; Character prefab was changed

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/UnitStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MultiPlayRPG
+{
+    public sealed class UnitStats : NetworkBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private int _maxHealth;
+        [SyncVar] private int _currentHealth;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        public override void OnStartAuthority()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        #endregion
+    }
+}
