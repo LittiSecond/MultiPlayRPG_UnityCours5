dec6f17453017c30ad24683f5010daebc72f736b
LittiSecond <breeze2882@yandex.ru>
2021-03-01 13:47:07 +0300

    Character of player prefab and it's scripts were changed

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/CharacterOfPlr.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace MultiPlayRPG
+{
+    public sealed class CharacterOfPlr : Unit
+    {
+        #region Fields
+
+        [SerializeField] GameObject _gfx;
+        [SerializeField] private float _revievDelay = 5.0f;
+
+        private Vector3 _startPosition;
+        private float _revievTime;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _startPosition = transform.position;
+            _revievTime = _revievDelay;
+        }
+
+        private void Update()
+        {
+            OnUpdate();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        protected override void OnDeadUpdate()
+        {
+            base.OnDeadUpdate();
+            if (_revievTime > 0)
+            {
+                _revievTime -= Time.deltaTime;
+            }
+            else
+            {
+                _revievTime = _revievDelay;
+                Revive();
+            }
+        }
+
+        protected override void Die()
+        {
+            base.Die();
+            _gfx.SetActive(false);
+        }
+
+        protected override void Revive()
+        {
+            base.Revive();
+            transform.position = _startPosition;
+            _gfx.SetActive(true);
+            if (isServer)
+            {
+                _motor.MoveToPoint(_startPosition);
+            }
+        }
+
+        public void SetMovePoint(Vector3 point)
+        {
+            if (_isAlive)
+            {
+                _motor.MoveToPoint(point);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MultiPlayRPG/Assets/Scripts/Other/PlayerCharacterLoader.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MultiPlayRPG
+{
+    public sealed class PlayerCharacterLoader : NetworkBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private GameObject _unitPrefab;
+        [SerializeField] private CharacterOfPlayerController _controller;
+
+        [SyncVar(hook = "HookUnitIdentity")] private NetworkIdentity _unitIdentity;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        //public override void OnStartServer()
+        //{
+        //    GameObject unit = Instantiate(_unitPrefab, transform.position, Quaternion.identity);
+        //    NetworkServer.SpawnWithClientAuthority(unit, gameObject);
+        //}
+
+        public override void OnStartAuthority()
+        {
+            //base.OnStartAuthority();
+            if (isServer)
+            {
+                GameObject unit = Instantiate(_unitPrefab, transform.position, Quaternion.identity);
+                NetworkServer.Spawn(unit);
+                _unitIdentity = unit.GetComponent<NetworkIdentity>();
+                _controller.SetCharacter(unit.GetComponent<CharacterOfPlr>(), true);
+            }
+            else
+            {
+                CmdCreatePlayer();
+            }
+        }
+
+        [Command]
+        public void CmdCreatePlayer()
+        {
+                GameObject unit = Instantiate(_unitPrefab, transform.position, Quaternion.identity);
+                NetworkServer.Spawn(unit);
+                _unitIdentity = unit.GetComponent<NetworkIdentity>();
+                _controller.SetCharacter(unit.GetComponent<CharacterOfPlr>(), false);
+        }
+
+        [ClientCallback]
+        void HookUnitIdentity(NetworkIdentity unit)
+        {
+            if (isLocalPlayer)
+            {
+                _unitIdentity = unit;
+                _controller.SetCharacter(unit.GetComponent<CharacterOfPlr>(), true);
+            }
+        }
+
+        #endregion
+    }
+}
