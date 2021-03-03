f32e9fbd5943f597c0d2191f29ab3e43656670bd
LittiSecond <breeze2882@yandex.ru>
2021-03-03 09:55:27 +0300

    Class Interactable was added

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MultiPlayRPG
+{
+    public class Interactable : NetworkBehaviour
+    {
+        #region Fields
+
+        public Transform _interactionTransform;
+        public float _radius = 2.0f;
+
+        private bool _hasInteract = true;
+
+        #endregion
+
+
+        #region Properties
+
+        public bool HasInteract
+        {
+            get
+            {
+                return _hasInteract;
+            }
+            protected set
+            {
+                _hasInteract = value;
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public virtual bool Interact(GameObject luser)
+        {
+            return false;
+        }
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(_interactionTransform.position, _radius);
+        }
+
+
+        #endregion
+
+
+    }
+}
