3eb6d6a0cdc768699c9c86e408260ce493515619
LittiSecond <breeze2882@yandex.ru>
2021-03-04 12:07:23 +0300

    Item collections was added

---
+++ MultiPlayRPG/Assets/Scripts/ScriptableObjects/ItemCollections.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    [CreateAssetMenu(fileName = "NewItemCollection", menuName = "CustomMenu/Inventory/ItemCollection")]
+    public class ItemCollections : ScriptableObject
+    {
+
+        public Item[] Items = new Item[0];
+
+    }
+}
