3b26f60567574800268112412069ba4898796696
LittiSecond <breeze2882@yandex.ru>
2021-03-04 11:35:48 +0300

    Update sword prefab

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/ItemPickUp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    public class ItemPickUp : Interactable
+    {
+        #region Fields
+
+        public Item ItemScriptableObject;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        #endregion
+
+
+        #region Methods
+
+        public override bool Interact(GameObject luser)
+        {
+            return PickUp(luser);
+        }
+
+        public bool PickUp(GameObject luser)
+        {
+            Debug.Log("ItemPickUp::PickUp: pick up " + ItemScriptableObject.Name);
+            Destroy(gameObject);
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ MultiPlayRPG/Assets/Scripts/ScriptableObjects/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    [CreateAssetMenu(fileName = "NewItem", menuName = "CustomMenu/Inventory/Item")]
+    public class Item : ScriptableObject
+    {
+        #region Fields
+
+        public string Name = "NewItem";
+        public Sprite Icon = null;
+        public ItemPickUp PickUpPrefab;
+
+        #endregion
+    }
+}
