1f3a9408505d94fa86381d8c5aacce6cd1fe0e97
LittiSecond <breeze2882@yandex.ru>
2021-03-04 15:58:20 +0300

    Inventory was added

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/Inventory.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+namespace MultiPlayRPG
+{
+    public class Inventory : NetworkBehaviour
+    {
+        #region Fields
+
+        public event SyncList<Item>.SyncListChanged OnItemChanged;
+
+        public int Space = 17;
+        public SyncListItem Items = new SyncListItem();
+
+        #endregion
+
+
+        #region UnityMethods
+
+        public override void OnStartLocalPlayer()
+        {
+            //base.OnStartLocalPlayer();
+            Items.Callback += ItemChanged;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public bool Add(Item item)
+        {
+            if (Items.Count < Space)
+            {
+                Items.Add(item);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Remove(Item item)
+        {
+            Items.Remove(item);
+        }
+
+        [Command]
+        void CmdRemoveItem(int index)
+        {
+            if (Items[index] != null)
+            {
+                Items.RemoveAt(index);
+            }
+        }
+
+        private void ItemChanged(SyncList<Item>.Operation op, int itemIndex)
+        {
+            OnItemChanged(op, itemIndex);
+        }
+
+        #endregion
+    }
+}
