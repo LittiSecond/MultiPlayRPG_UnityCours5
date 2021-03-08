5b8ab1208631a27b4d87b48041d62f159c90ad7a
LittiSecond <breeze2882@yandex.ru>
2021-03-08 11:37:24 +0300

    Added class EquipmentItem and one more sword

---
+++ MultiPlayRPG/Assets/Scripts/ScriptableObjects/EquiomentItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    [CreateAssetMenu(fileName = "New equipment", menuName = "CustomMenu/Inventory/Equipment")]
+    public class EquiomentItem : Item
+    {
+        #region Fields
+
+        public EquipmentSlotType EquipmentSlot;
+
+        public int DamageModifer;
+        public int ArmorModifer;
+        public int SpeedModifer;
+
+        #endregion
+
+
+        #region Methods
+
+        public override void Use()
+        {
+
+
+            base.Use();
+        }
+
+        #endregion
+    }
+}
