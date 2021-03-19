8b8a642ab9586366b417a50e1104bf34bd7419b6
LittiSecond <breeze2882@yandex.ru>
2021-03-19 10:47:16 +0300

    Upgrade StatsUI, StatsItem and stats at UI

--- MultiPlayRPG/Assets/Scripts/Ui/StatItemUI.cs
+++ MultiPlayRPG/Assets/Scripts/Ui/StatItemUI.cs
@@ -6,12 +6,26 @@ namespace MultiPlayRPG
 {
     public class StatItemUI : MonoBehaviour
     {
+        #region Fields
+
         [SerializeField] private Text _value;
+        [SerializeField] private Button _upgradeButton;
+
+        #endregion
+
+
+        #region Methods
 
         public void ChangeStat(int dtat)
         {
             _value.text = dtat.ToString();
         }
 
+        public void SetUpgradable(bool isUpgradeable)
+        {
+            _upgradeButton.gameObject.SetActive(isUpgradeable);
+        }
+
+        #endregion
     }
 }
