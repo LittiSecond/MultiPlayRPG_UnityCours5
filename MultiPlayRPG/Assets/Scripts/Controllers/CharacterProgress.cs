5034e9c4f61f43496c904180d9f6a2f4c32499d6
LittiSecond <breeze2882@yandex.ru>
2021-03-19 09:12:48 +0300

    CharacterProgress was added

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/CharacterProgress.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace MultiPlayRPG
+{
+    public class CharacterProgress : MonoBehaviour
+    {
+        #region Fields
+
+        private const float EXPERIENCE_STEP = 100.0f;
+        private const int STAT_POINTS_PER_LEVEL = 3;
+
+        private float _expa;
+        private float _nextLevelExp;
+        private int _level;
+        private int _statPoints;
+
+        #endregion
+
+
+        #region Methods
+
+        public void AddExp(float addExpa)
+        {
+            _expa += addExpa;
+            while (_expa >= _nextLevelExp)
+            {
+                _expa -= _nextLevelExp;
+                LevelUp();
+            }
+        }
+
+        private void LevelUp()
+        {
+            _level++;
+            _nextLevelExp += EXPERIENCE_STEP;
+            _statPoints += STAT_POINTS_PER_LEVEL;
+        }
+
+        public bool RemoveStatPoint()
+        {
+            if (_statPoints > 0)
+            {
+                _statPoints--;
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
