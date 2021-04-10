1f12ff69b5b7df013a95e496597e7b4755623929
LittiSecond <breeze2882@yandex.ru>
2021-04-10 10:18:57 +0300

    Class Skill was added

---
+++ MultiPlayRPG/Assets/Scripts/Controllers/Skill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MultiPlayRPG
+{
+    public class Skill :NetworkBehaviour
+    {
+
+        public Sprite Icon;
+
+        [SerializeField] float castTime = 1f;
+        [SerializeField] float cooldown = 1f;
+
+    }
+}
