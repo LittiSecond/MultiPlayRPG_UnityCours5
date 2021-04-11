672eab6539f7a6474f3442cd5ab53369001531c3
LittiSecond <breeze2882@yandex.ru>
2021-04-11 13:42:07 +0300

    Add skills for first character

---
+++ MultiPlayRPG/Assets/Scripts/Skills/FrontWarpSkill.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    public class FrontWarpSkill : Skill
+    {
+        #region Fields
+
+        [SerializeField] private float _warpDistance = 7.0f;
+
+        #endregion
+
+
+        #region Methods
+
+        protected override void OnUse()
+        {
+            if (isServer)
+            {
+                _unit.Motor.StopFollowingTarget();
+            }
+            base.OnUse();
+        }
+
+        protected override void OnCastComplete()
+        {
+            if (isServer)
+            {
+                _unit.transform.Translate(Vector3.forward * _warpDistance);
+                _unit.Motor.StopFollowingTarget();
+            }
+            base.OnCastComplete();
+        }
+
+        #endregion
+    }
+}
---
+++ MultiPlayRPG/Assets/Scripts/Skills/HealOneselfSkill.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace MultiPlayRPG
+{
+    public class HealOneselfSkill : Skill
+    {
+        [SerializeField] private int _healAmount = 10;
+        [SerializeField] private ParticleSystem _particle;
+
+        protected override void OnCastComplete()
+        {
+            if (isServer)
+            {
+                _unit.Stats.TakeHealing(_healAmount);
+            }
+            else 
+            {
+                _particle.Play();
+            }
+            base.OnCastComplete();
+        }
+
+    }
+}
